e144c024b215c667bf7bb7c0a19aa68506d59b28
Nick Martinez <habbarazzer@gmail.com>
2017-03-26 01:49:27 -0400

    started converting client into client-server

--- c/client.c
+++ c/client.c
@@ -22,7 +22,10 @@
 #define SERVER_ADDR "127.0.0.1"
 #define NUM_MESSAGES 10
 
-void *handle_message(void *socket);
+size_t current_value = 1;
+
+void diep(char* s);
+bool checksum(char* message);
 
 /**
  * Handles any messages sent from a client.
@@ -47,8 +50,12 @@ void *handle_message(void *socket)
       // client has closed the connection
       pthread_exit(NULL);
     }
+    
+    // corruption check
+    // bool corrupt = !checksum(recv_buffer);
 
     // print the contents of the message
+    // if( !corrupt ) { ... }
     printf("data - %d%d, source - %d", recv_buffer[DATA_OFFSET], recv_buffer[DATA_OFFSET+1], 
         recv_buffer[SOURCE_OFFSET]);
 
@@ -57,6 +64,25 @@ void *handle_message(void *socket)
       diep("client - send() inside handle_message thread");
     }
   }
+}
+
+/**
+ * Checks the message to see if it is corrupted.
+ *
+ * return:
+ *   true if not corrupted
+ */
+bool checksum(char* message)
+{
+  //TODO
+}
+
+/**
+ * Sends a message to client selected at random every two seconds. 
+ */
+void *send_message(void) 
+{
+  // every two seconds - send message to client randomly selected
 }
 
 /**
@@ -73,63 +99,60 @@ void diep(char* s)
 
 int main(int argc, char *argv[])
 {
+    // start the send message loop
+    pthread_t thread;
+    int status = 0;
+    if( (status = pthread_create(&thread, NULL, send_message, NULL)) != 0 )
+    {
+        fprintf(stderr, "client - error creating thread! error code = %d\n", status);
+        exit(1);
+    }
+
     int server_socket = 0;
 
     struct sockaddr_in server_info;
+    struct sockaddr_in client_info; 
 
+    // obtain a socket for the server
     if( (server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1 )
-	{	
-        diep("client - socket()");
-	}
-
-    memset(&server_info, 0, sizeof(server_info));
-
-    server_info.sin_family = AF_INET;
-    server_info.sin_addr.s_addr = inet_addr(SERVER_ADDR);
-    server_info.sin_port = htons(PORT); 
-
-    // establish connection with server
-    if( connect(server_socket, (struct sockaddr*)&server_info, sizeof(server_info)) == -1 )
-	{
-        diep("client - connect()"); 
-	}
-
-    // send 10 messages to be processed by server
-    size_t i;
-    char send_buffer[MAX_BUFFER_SIZE] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
-    char receive_buffer[MAX_BUFFER_SIZE] = {0};
-    printf("C client is running...\n");
-    for( i = 0; i < NUM_MESSAGES; ++i ) 
     {
-      size_t j;
-      printf("Sent - ");
-      for( j = 0; j < MAX_BUFFER_SIZE; ++j ) 
-      {
-        printf("%c", send_buffer[j]);
-      }
-      printf("\n");
-
-      if( send(server_socket, send_buffer, sizeof send_buffer, 0) == -1 ) 
-      {
-        diep("client - send()");
-      }
-
-      if( recv(server_socket, receive_buffer, sizeof receive_buffer, 0) == -1 ) 
-      {
-        diep("client - recv()");
-      }
-
-      printf("Received - ");
-      for( j = 0; j < MAX_BUFFER_SIZE; ++j ) 
-      {
-        printf("%c", receive_buffer[j]);
-      }
-      printf("\n");
+      diep("client - socket() inside main");
     }
-    printf("C client done\n");
-    
-    // close connection with server
-    close(server_socket);
+
+    memset(&server_info, 0, sizeof server_info);
+    server_info.sin_family = AF_INET;
+    server_info.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_info.sin_port = htons(PORT);
+
+	// bind the server to port
+    if( bind(server_socket, (struct sockaddr*)&server_info, sizeof server_info) == -1 ) 
+	{
+		diep("client - bind() inside main");
+	} 
+
+	// listen for incoming connections
+    if( listen(server_socket, BACKLOG) == -1 ) 
+	{
+		diep("client - listen() inside main");
+	}
+
+    printf("C server is listening...\n");
+    while(1)
+    {
+        // accept request and create new thread to service that request
+        int client_len = sizeof client_info;
+        int connection_socket = accept(server_socket, (struct sockaddr*)&client_info, 
+            (socklen_t*)&client_len); 
+
+        printf("accepted\n");
+        pthread_t thread;
+        int status = 0;
+        if( (status = pthread_create(&thread, NULL, handle_request_t, (void *)&connection_socket)) != 0 ) 
+        {
+          fprintf(stderr, "client - error creating thread! error code = %d\n", status);
+          exit(1);
+        }
+    }
 
 	return EXIT_SUCCESS;
 }
