d09ae180dafcb2f89fb39d2645542325cc388164
Nick Martinez <habbarazzer@gmail.com>
2017-03-31 23:34:21 -0400

    change error messages; added thread for router connections

--- c/router.c
+++ c/router.c
@@ -14,14 +14,16 @@
 #include <sys/types.h>
 #include <time.h> 
 #include <pthread.h>
-#include 'router_funcs.h'
+#include "router_funcs.h"
 
-#define PORT 4446
+#define CLIENT_PORT 8000
+#define ROUTER_PORT 8080
 #define BACKLOG 10
 #define NUM_ROUTERS 4
-#define TABLE_LEN ('a' + NUM_ROUTERS)
+#define TABLE_LEN ('A' + NUM_ROUTERS)
 
-const * char const ROUTING_TABLE[TABLE_LEN] = { ['a'] = "127.0.0.1", ['b'] = "other1", ['c'] = "other2", ['d'] = "other3" };
+const * char const ROUTING_TABLE[TABLE_LEN] = { ['A'] = "127.0.0.1", ['B'] = "other1", 
+    ['C'] = "other2", ['D'] = "other3" };
 
 void *handle_request_t(void *socket);
 void reverse(char *buffer, size_t len);
@@ -34,29 +36,63 @@ void reverse(char *buffer, size_t len);
  */
 void *handle_request_t(void *socket)
 {
-  while( 1 ) 
-  {
-    char recv_buffer[MAX_BUFFER_SIZE] = {0};
-    int status;
-    int client_socket = *((int*) socket);
-    if( (status = recv(client_socket, recv_buffer, sizeof recv_buffer, 0)) == -1 ) 
+    // this thread will accept messages from the local client or it's neighboring routers
+    while( 1 ) 
     {
-      diep("server - recv()");
-    }
+        char recv_buffer[MAX_BUFFER_SIZE] = {0};
+        int status;
+        int client_socket = *((int*) socket);
+        if( (status = recv(client_socket, recv_buffer, sizeof recv_buffer, 0)) == -1 ) 
+        {
+            diep("router - recv() in handle_request_t");
+        }
 
-    if( status == 0 ) 
-    {
-      // client has closed the connection
-      pthread_exit(NULL);
-    }
+        if( status == 0 ) 
+        {
+            // client has closed the connection
+            pthread_exit(NULL);
+        }
 
-    reverse(recv_buffer, sizeof recv_buffer);
-    if( send(client_socket, recv_buffer, sizeof recv_buffer, 0) == -1 ) 
-    {
-      diep("server - send()");
+        reverse(recv_buffer, sizeof recv_buffer);
+        if( send(client_socket, recv_buffer, sizeof recv_buffer, 0) == -1 ) 
+        {
+            diep("router - send() in handle_request_t");
+        }
     }
-  }
 }
+
+
+/**
+ * Router request handler.
+ *
+ * params:
+ *  socket - the socket interface to the router connection
+ */
+void *handle_request_t(void *socket)
+{
+    // this thread will accept messages from the local client or it's neighboring routers
+    while( 1 ) 
+    {
+        char recv_buffer[MAX_BUFFER_SIZE] = {0};
+        int status;
+        int client_socket = *((int*) socket);
+        if( (status = recv(client_socket, recv_buffer, sizeof recv_buffer, 0)) == -1 ) 
+        {
+            diep("router - recv() in handle_request_t");
+        }
+
+        if( status == 0 ) 
+        {
+            // client has closed the connection
+            pthread_exit(NULL);
+        }
+
+        reverse(recv_buffer, sizeof recv_buffer);
+        if( send(client_socket, recv_buffer, sizeof recv_buffer, 0) == -1 ) 
+        {
+            diep("router - send() in handle_request_t");
+        }
+    }
 
 int main(int argc, char *argv[])
 {
@@ -68,7 +104,7 @@ int main(int argc, char *argv[])
 	// obtain a socket for the server
     if( (server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1 )
 	{
-        diep("server - socket() in main");
+        diep("router - socket() in main");
 	}
 
     memset(&server_info, 0, sizeof server_info);
@@ -79,16 +115,16 @@ int main(int argc, char *argv[])
 	// bind the server to port
     if( bind(server_socket, (struct sockaddr*)&server_info, sizeof server_info) == -1 ) 
 	{
-		diep("server - bind() in main");
+		diep("router - bind() in main");
 	} 
 
 	// listen for incoming connections
     if( listen(server_socket, BACKLOG) == -1 ) 
 	{
-		diep("server - listen() in main");
+		diep("router - listen() in main");
 	}
 
-    printf("server is listening...\n");
+    printf("router is listening...\n");
     while(1)
     {
         // accept request and create new thread to service that request
@@ -101,7 +137,7 @@ int main(int argc, char *argv[])
         int status = 0;
         if( (status = pthread_create(&thread, NULL, handle_request_t, (void *)&connection_socket)) != 0 ) 
         {
-          fprintf(stderr, "server - error creating thread! error code = %d\n", status);
+          fprintf(stderr, "router - error creating thread in main! error code = %d\n", status);
           exit(1);
         }
      }
